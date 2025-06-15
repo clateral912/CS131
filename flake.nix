9486b7da7a8a23807fe860711ded56defc05daea
kiturolin <linkzh2024@shanghaitech.edu.cn>
2025-06-16 01:50:01 +0800

    hw2~6 template initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages =
+          with pkgs;
+          [
+            gnumake
+            zip
+          ]
+          ++ (with ocaml-ng.ocamlPackages_4_14; [
+            dune_3
+            num
+            ocaml
+            ocaml-lsp
+            ocamlformat_0_26_1
+            utop
+          ]);
+      };
+
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
+}
